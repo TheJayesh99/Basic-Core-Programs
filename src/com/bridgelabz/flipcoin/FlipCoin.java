6742f876e12de921845d1215838a9fe1e12f8adc
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-23 18:01:11 +0530

    Added : initial files

---
+++ src/com/bridgelabz/flipcoin/FlipCoin.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.flipcoin;
+
+import java.util.Scanner;
+
+public class FlipCoin {
+public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter number of times you have to flip a coin : ");
+		int numberOfFlips = scanner.nextInt();
+		scanner.close();
+		
+		if (numberOfFlips <= 0) 
+		{
+			System.err.println("Please enter a postive integer");
+		}
+		
+		else 
+		{
+			//method to flip a coin
+			int countHeads = flip(numberOfFlips);
+			double percentOfHeads = (countHeads*100)/numberOfFlips;
+			double percentOfTails = 100-percentOfHeads;
+			System.out.println("heads percentage : "+ percentOfHeads);
+			System.out.println("Tails percentage : "+ percentOfTails);
+		}
+	}
+
+
+
+	public static int flip(int numOfFlips) 
+	{
+		int countHeads = 0;
+		for (int i = 0; i < numOfFlips; i++) 
+		{
+			//generating a random number between 0 and 1
+			if (Math.random() > 0.5) 
+			{
+				countHeads++;
+			}
+		}
+		return countHeads;
+	}
+}
