31bb6c7037963621a44f5aeb28b77ffa8ae6fb0b
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-23 21:17:28 +0530

    Added : Program to Find Prime factors

---
+++ src/com/bridgelabz/primeFactorization/PrimeFactorization.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.primeFactorization;
+
+import java.util.Scanner;
+
+public class PrimeFactorization {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter The Number Whose prime factors is to Find  ");
+		int number = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println("The factors are");
+		
+		for(int i=2 ; i<=number ;i++)
+		{
+			int is_prime = 0;
+			
+			for (int j = 2 ; j<i ;j++ )
+			{
+				int remainder = i % j ;
+				if (remainder == 0)
+				{
+					is_prime ++ ;
+				}
+				
+					
+			}
+			
+			if (is_prime == 0)
+			{
+				while ((number % i) == 0)
+				{
+					
+					System.out.println(i);
+					number = number/i;
+				}
+			}
+		}
+	}
+}
