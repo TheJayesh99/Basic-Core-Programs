5896bc79d18ed8294a1061b5b034acd7b62c3203
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-23 19:57:23 +0530

    Added : Program to Check entered Number is even or odd

---
+++ src/com/bridgelabz/evenOdd/EvenOdd.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.evenOdd;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the  number to check weather its  even or odd");
+		int number = scanner.nextInt();
+		scanner.close();
+		if (number % 2 ==0 )
+		{
+			System.out.println("The number is EVEN	");
+		}
+		else
+		{
+			System.out.println("The number is ODD");
+		}
+	}
+}
